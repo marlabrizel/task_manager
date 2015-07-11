99896e5178f3280fb2f3813ccd7d7f36a5faa354
Marla Brizel <marlabrizel@gmail.com>
2015-07-11 10:05:18 -0600

    WIP - replace YAML db with sqlite3

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -1,4 +1,3 @@
-require 'yaml/store'
 require 'sequel'
 require 'sqlite3'
 
@@ -7,56 +6,73 @@ class TaskManager
     if ENV["TASK_MANAGER_ENV"] == 'test'
       @database ||= Sequel.sqlite("db/task_manager_test.sqlite3")
     else
-      @database ||= Sequel.sqlite("db/task_manager.sqlite3")
+      @database ||= Sequel.sqlite("db/task_manager_development.sqlite3")
     end
   end
 
   def self.create(task)
-    database.transaction do
-      database['tasks'] ||= []
-      database['total'] ||= 0
-      database['total'] += 1
-      database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
+    begin
+      dataset.insert(title:       task[:title],
+                     description: task[:description])
+    rescue
+      return false
     end
+    Task.new(dataset.to_a.last)
+    # database.transaction do
+      # database['tasks'] ||= []
+      # # database['total'] ||= 0
+      # # database['total'] += 1
+      # database['tasks'] << { "id" => database['total'],
+      #                        "title" => task[:title],
+      #                        "description" => task[:description] }
+    # end
   end
 
-  def self.raw_tasks
-    database.transaction do
-      database['tasks'] || []
-    end
-  end
+  # def self.raw_tasks
+  #   database.transaction do
+  #     database['tasks'] || []
+  #   end
+  # end
 
   def self.all
-    raw_tasks.map { |data| Task.new(data) }
+    dataset.to_a.map { |data| Task.new(data) }
+    # raw_tasks.map { |data| Task.new(data) }
   end
 
-  def self.raw_task(id)
-    raw_tasks.find { |task| task["id"] == id }
-  end
+  # def self.raw_task(id)
+  #   raw_tasks.find { |task| task["id"] == id }
+  # end
 
   def self.find(id)
-    Task.new(raw_task(id))
+    Task.new(dataset.where(id: id).first)
+    # Task.new(raw_task(id))
   end
 
   def self.update(id, task)
-    database.transaction do
-      target = database['tasks'].find { |data| data["id"] == id }
-      target["title"] = task[:title]
-      target["description"] = task[:description]
-    end
+    target = dataset.where(id: id).update(task)
+    # database.transaction do
+    #   target = database['tasks'].find { |data| data["id"] == id }
+    #   target["title"] = task[:title]
+    #   target["description"] = task[:description]
+    # end
   end
 
   def self.delete(id)
-    database.transaction do
-      database['tasks'].delete_if { |task| task["id"] == id }
-    end
+    dataset.where(id: id).delete
+    # database.transaction do
+    #   database['tasks'].delete_if { |task| task["id"] == id }
+    # end
   end
 
   def self.delete_all
-    database.from(:tasks).delete 
+    dataset.delete
     # database.transaction do
     #   database['tasks'] = []
     #   database['total'] = 0
     # end
+  end
+
+  def self.dataset
+    database.from(:tasks)
   end
 end
